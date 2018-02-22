f606bcad443da631fe43ab8cbb7fdb74d05afdb7
Esteban Herrera <estebanhb2@gmail.com>
2018-02-21 19:32:45 -0600

    Upgrade to react-vr 2.0.0

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var path = require('path');
-var blacklist = require('./node_modules/react-native/packager/blacklist');
+var blacklist = require('metro-bundler/src/blacklist');
 
 var config = {
   getProjectRoots() {
